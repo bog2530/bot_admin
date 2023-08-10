5282c6e972faa2c47277cfc5eab3ad6aeec47751
Bogdan Shumskiy <bog2530@gmail.com>
2023-08-10 20:44:59 +0300

    start

--- config/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-url = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "app.db")
-# url = "postgresql://postgres:postgres@127.0.0.1:5433/temp"
